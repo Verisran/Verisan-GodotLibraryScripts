47613755b05ee66144e60b685d1901b0ca66ce87
Verisran <oskardobbz@gmail.com>
2025-01-20 16:29:23 +0000

    Updated completion method, Added task complete

--- Threaded Load & Instance/ThreadLoadRequest.gd
+++ Threaded Load & Instance/ThreadLoadRequest.gd
@@ -1,27 +1,23 @@
 extends Node
 
-enum AddMode{
-		AS_CHILD = 0,
-		TO_POOL = 1
-}
-
-#--------------------------------------------------------------------
 signal load_request_complete
 
 func _ready() -> void:
-	load_request_complete.connect(send_complete_request)
+	load_request_complete.connect(call_complete_request)
 
 #Request fucntions
 ##request_path is the string path of the resource you wish to load and instance
 ##amount is how many copies you wish to instance at once
 ##return_loc is the node that the instances will be sent to
-##add_mode, uses the add_mode enum, by default nodes are added as children, the secondary mode is only usable when the target node has a reference to the wep_manager
-func request(request_path: String, amount: int, return_loc: Node, add_mode: AddMode = AddMode.AS_CHILD)->void:
-	WorkerThreadPool.add_task(loader.bind(request_path, amount, return_loc, add_mode))
-
+##add_func is the callable that will recieve the request results as an Array[Node] and add_target
+func request(request_path: String, amount: int, add_location: Node, add_func: Callable = default_add)->void:
+	var task_id: int = WorkerThreadPool.add_task(loader.bind(request_path, amount, add_location, add_func))
+	await load_request_complete
+	WorkerThreadPool.wait_for_task_completion(task_id)
+	
 #Load function
 ##The function is used inside the WorkThreadPool, simply loads instantiates n amount of requested objects, emits load_request_complete when it finishes
-func loader(req: String, req_amt: int, target: Node, add_mode: int)->void:
+func loader(req: String, req_amt: int, add_location: Node, add_func: Callable)->void:
 	var return_array: Array[Node]
 	#pop request, amt, and target node to send instanced items to
 	var requested_item: Resource = load(req)
@@ -29,16 +25,16 @@ func loader(req: String, req_amt: int, target: Node, add_mode: int)->void:
 	for j in range(req_amt):
 		return_array.append(requested_item.instantiate())
 	#Emits signal once complete
-	call_deferred("emit_signal", "load_request_complete", return_array, target, add_mode)
+	call_deferred("emit_signal", "load_request_complete", return_array, add_location, add_func)
 
 #Add
 ##Connected to load_request_complete, sends the newly instanced nodes to their target location. I can be set to either add to child of target or to add to pool of wep_manager [wip]
-func send_complete_request(items: Array[Node], add_target: Node, add_mode: int)->void:
-	if(add_mode == AddMode.AS_CHILD):
-		for item in items:
-			add_target.add_child(item)
-	elif(add_mode == AddMode.TO_POOL):
-		print("'AddMode.TO_POOL' Doesnt Function at this moment")
-		for item in items:
-			pass
-			#add_target.wep_manager.pool.append(item)
+func call_complete_request(items: Array[Node], add_location: Node, add_func: Callable)->void:
+	#WorkerThreadPool.wait_for_task_completion()
+	add_func.call(items, add_location)
+
+func default_add(items: Array[Node], add_location: Node)->void:
+	#Ensures runs on next frame only
+	await get_tree().physics_frame
+	for item in items:
+		add_location.add_child(item)
