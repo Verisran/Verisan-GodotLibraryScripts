b4f04e68ef963e34152da91ddc626cbb7a33d582
Verisran <oskardobbz@gmail.com>
2025-01-23 07:15:42 +0000

    removed 5 am code

--- Physics Related/ShortPhysics2D.gd
+++ Physics Related/ShortPhysics2D.gd
@@ -15,15 +15,14 @@ func ray(myself: Node2D, from: Vector2, direction: Vector2, length: float, layer
 	query.collide_with_areas = areas
 	return space_state.intersect_ray(query)
 
-func shape(myself: Node2D, shape: Shape2D, from: Vector2, direction: Vector2, length: float, layers: int, max_results: int = 1, bodies: bool = true, areas: bool = false, space_state: PhysicsDirectSpaceState2D = null)->Array[Dictionary]:
+func shape(myself: Node2D, shape2d: Shape2D, layers: int, max_results: int = 1, bodies: bool = true, areas: bool = false, space_state: PhysicsDirectSpaceState2D = null)->Array[Dictionary]:
 	if(space_state == null):
 		space_state = myself.get_world_2d().direct_space_state
 	var query: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
-	var set_shape: Shape2D = shape
-	query.set_collision_mask(layers); 
-	query.set_shape(shape); 
-	query.set_transform(myself.transform); 
-	query.set_margin(0.01); 
+	query.set_collision_mask(layers)
+	query.set_shape(shape2d)
+	query.set_transform(myself.transform)
+	query.set_margin(0.01)
 	query.collide_with_bodies = bodies
 	query.collide_with_areas = areas
 	
